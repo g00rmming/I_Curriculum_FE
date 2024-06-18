01ead067793f21f9b1a7e2446120dc4759f4fd06
LEE K.S <108475935+KSLEE19@users.noreply.github.com>
2024-06-18 23:12:15 +0900

    temp

--- vue.config.js
+++ vue.config.js
@@ -44,7 +44,7 @@ module.exports = defineConfig({
         changeOrigin: true,
       },
       '/login': {
-        target: `${process.env.VUE_APP_EC2_IP}`, // 프록시할 대상 서버의 주소
+        target: `${springbootUrl}`, // 프록시할 대상 서버의 주소
         changeOrigin: true,
       }
     }
