010d2a0759fa582f86f48171a4e924725e7d022d
gimdongha <d11210920@gmail.com>
2024-06-19 06:01:17 +0900

    a

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ const springport = dev === 'local' ? process.env.SPRING_PORT : process.env.SOCKE
 
 // 개발환경 - 아래 프록시를 로컬환경에서 실행 했을 경우 사용
 
-const springbootUrl = `http://13.214.220.207`
+const springbootUrl = `http://47.129.11.199`
 
 module.exports = defineConfig({
   // 기본 경로를 명시적으로 설정합니다.
@@ -30,11 +30,11 @@ module.exports = defineConfig({
     headers: { 'Access-Control-Allow-Origin': '*' },
     // 프록시 설정
     proxy: {
-      // '/api': {
-      //   target: 'https://ek66mfbfpfa23yw5qfymhrh6ti0vnirc.lambda-url.ap-southeast-1.on.aws',
-      //   changeOrigin: true,
-      //   pathRewrite: { '^/api': '' },
-      // },
+      '/api': {
+        target: 'https://ek66mfbfpfa23yw5qfymhrh6ti0vnirc.lambda-url.ap-southeast-1.on.aws',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
+      },
       // 프록시 요청을 보낼 api의 시작 부분 
       '/v1': {
         target: process.env.VUE_APP_SPRING_HOSTNAME, // 프록시할 대상 서버의 주소
